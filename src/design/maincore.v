1ae29b03a07e42e1e2cfc300f3d2e17d0b585a73
Robin Dorst <robin@dorsti.de>
2025-01-02 19:41:44 +0100

    add porches

--- src/design/maincore.v
+++ src/design/maincore.v
@@ -15,6 +15,17 @@ parameter ScreenY = 768;  //768
 parameter BlankingVertical = 12;
 parameter BlankingHorizontal = 174;
 
+parameter FrontPorchHorizontal = 30;
+parameter BackPorchHorizontal = 30;
+parameter SyncPulseHorizontal = 114;
+
+parameter FrontPorchVertical = 2;
+parameter BackPorchVertical = 2;
+parameter SyncPulseVertical = 8;
+
+parameter SyncOn = 1;
+parameter SyncOff = 0;
+
 parameter integer     D = 3 ;				// Set the number of outputs per channel to be 3
 parameter integer     N = 1 ;				// Set the number of channels to be 1
 
@@ -34,13 +45,13 @@ wire		txclk_div ;
 wire		not_tx_mmcm_lckd ;	
 wire		tx_mmcm_lckd ;
 
-reg HSync = 1, VSync = 1, DataEnable = 0;
+reg HSync = SyncOff;
+reg VSync = SyncOff;
+reg DataEnable = 0;
 
 
 reg [10:0] PosX = 0;
 reg [10:0] PosY = 0;
-
-reg [7:0] SendFrames = 0;
 
 assign reset = ~rst;
 
@@ -49,8 +60,8 @@ assign led_pwm = 1;
 
 // Input Clock Buffer
 BUFG bg_ref (
-    .I             (clk),
-    .O             (clk100_g)
+    .I		(clk),
+    .O      (clk100_g)
 	);
 
 // Pixel Clock Generator
@@ -64,18 +75,14 @@ assign VideoData[20:14]	= {Blue[2],Blue[3],Blue[4],Blue[5],HSync,VSync,DataEnabl
 assign VideoData[13:7]  = {Green[1],Green[2],Green[3],Green[4],Green[5],Blue[0],Blue[1]};
 assign VideoData[6:0]	= {Red[0],Red[1],Red[2],Red[3],Red[4],Red[5],Green[0]};
 
-
-reg [5:0] Parallax = 0;
-
-
 // Clock Input
 
 clock_generator_pll_7_to_1_diff_ddr #(
 	.PIXEL_CLOCK		("BUF_G"),
 	.INTER_CLOCK 		("BUF_G"),
-	.TX_CLOCK		("BUF_G"),
-	.USE_PLL		("FALSE"),
-	.MMCM_MODE		(2),				// Parameter to set multiplier for MMCM to get VCO in correct operating range. 1 multiplies input clock by 7, 2 multiplies clock by 14, etc
+	.TX_CLOCK			("BUF_G"),
+	.USE_PLL			("FALSE"),
+	.MMCM_MODE			(2),				// Parameter to set multiplier for MMCM to get VCO in correct operating range. 1 multiplies input clock by 7, 2 multiplies clock by 14, etc
 	.CLKIN_PERIOD 		(13.889))
 clkgen (                        
 	.reset			(reset),
@@ -92,17 +99,17 @@ assign not_tx_mmcm_lckd = ~tx_mmcm_lckd ;
 n_x_serdes_7_to_1_diff_ddr #(
       	.D			(D),
       	.N			(N),				// 1 channel
-	.DATA_FORMAT 		("PER_CLOCK")) 			// PER_CLOCK or PER_CHANL data formatting
+	.DATA_FORMAT 	("PER_CLOCK")) 			// PER_CLOCK or PER_CHANL data formatting
 dataout (                      
-	.dataout_p  		(dataout_p),
-	.dataout_n  		(dataout_n),
+	.dataout_p  	(dataout_p),
+	.dataout_n  	(dataout_n),
 	.clkout_p  		(clkout_p),
 	.clkout_n  		(clkout_n),
 	.txclk    		(txclk),
-	.txclk_div    		(txclk_div),
+	.txclk_div    	(txclk_div),
 	.pixel_clk		(pixel_clk),
 	.reset   		(not_tx_mmcm_lckd),
-	.clk_pattern  		(7'b1100011),			// Transmit a constant to make the clock
+	.clk_pattern  	(7'b1100011),			// Transmit a constant to make the clock
 	.datain  		(VideoData)
 	);
 
@@ -111,52 +118,65 @@ dataout (
 //Cycle Generator
 always @(posedge pixel_clk)
 begin
-			//Sync Generator
+			// Increment horizontal position
 			PosX <= PosX + 1;
 			
-			// End of Screen in X				
-			if(PosX == ScreenX)
-			begin
-					DataEnable	 	<= 0;
-					HSync 			<= 0;
+			// Start horizontal blanking
+			if (PosX == ScreenX) begin
+				DataEnable <= 0;
 			end
 			
-			// Start of line
-			if((PosX == 0) & (PosY < ScreenY))
-					DataEnable 	<= 1;
+			// Start horizontal sync
+			if (PosX == ScreenX + FrontPorchHorizontal) begin
+				HSync <= SyncOn;
+			end
+
+			// End horizontal sync
+			if (PosX == ScreenX + FrontPorchHorizontal + SyncPulseHorizontal) begin
+				HSync <= SyncOff;
+			end
 					
 			// End of line						
 			if(PosX == (ScreenX+BlankingHorizontal))
 			begin
-			        HSync 			<= 1;
-			        
-			        // End of Screen in Y
+			        PosX <= 0;
+					PosY <= PosY + 1;
+
+        			// Start vertical blanking
 					if(PosY == ScreenY)
 					begin
-							VSync 		<= 0;
 							DataEnable	<= 0;
 					end
 					
-					// End of Lines in Y
+					// Start vertical sync
+					if(PosY == ScreenY + FrontPorchVertical)
+					begin
+							VSync <= SyncOn;
+					end
+
+					// End vertical sync
+					if(PosY == ScreenY + FrontPorchVertical + SyncPulseVertical)
+					begin
+							VSync <= SyncOff;
+					end
+
+					// End of frame
 					if(PosY == (ScreenY+BlankingVertical))
 					begin
-							VSync 		<= 1;
-							PosY 	<= 0;
-							PosX 	<= 0;
+							PosY <= 0;
+							VSync <= SyncOff;
 					end
-					else
-					begin
-					    	PosY <= PosY +1;
-					end
-					
-					PosX 	<= 0;
-           end               
+           end
+		   else if (PosX == 0 && PosY < ScreenY)
+		   begin
+				DataEnable <= 1;
+		   end             
 end
 
 //Video Generator
 always @(posedge pixel_clk)
 begin
-	if(PosX <= 100 && DataEnable)
+	if(DataEnable)
 		begin
 				Blue 			<= 63;
 				Red 			<= 0;
